2c9ac7da1a00d3ea70afc85670aa18e0d3df66f3
Stuart Carnie <stuart.carnie@gmail.com>
2018-01-10 20:08:11 -0700

    added larger arrays

--- README.md
+++ README.md
@@ -14,9 +14,12 @@ Benchmark various sum implementations `[1000]float64`.
 * `SumFloat64Go` is an equivalent loop in Go
 
 ```
-BenchmarkSumFloat64AVX-8   	 5000000	       255 ns/op	31276.62 MB/s
-BenchmarkSumFloat64b-8     	20000000	        84.4 ns/op	94735.93 MB/s
-BenchmarkSumFloat64Go-8    	 1000000	      1150 ns/op	6956.41 MB/s
+BenchmarkSumFloat64AVX_1000-8    	 5000000	       260 ns/op	30675.32 MB/s
+BenchmarkSumFloat64AVX_10000-8   	  500000	      3015 ns/op	26529.54 MB/s
+BenchmarkSumFloat64b_1000-8      	20000000	      88.6 ns/op	90307.32 MB/s
+BenchmarkSumFloat64b_10000-8     	 2000000	       856 ns/op	93395.87 MB/s
+BenchmarkSumFloat64Go_1000-8     	 1000000	      1215 ns/op	6580.11 MB/s
+BenchmarkSumFloat64Go_10000-8    	  100000	     12243 ns/op	6534.24 MB/s
 ```
 
 `SumFloat64b` is just a simple C-loop, letting the compiler auto-vectorize.
--- add_test.go
+++ add_test.go
@@ -6,21 +6,21 @@ import (
 )
 
 func TestSumFloat64AVX(t *testing.T) {
-	buf := makeBuffer(1000)
+	buf := makeBuffer(10000)
 	res := SumFloat64AVX(buf)
-	assert.Equal(t, res, float64(499500.0))
+	assert.Equal(t, res, float64(49995000.0))
 }
 
 func TestSumFloat64b(t *testing.T) {
-	buf := makeBuffer(1000)
+	buf := makeBuffer(10000)
 	res := SumFloat64b(buf)
-	assert.Equal(t, res, float64(499500.0))
+	assert.Equal(t, res, float64(49995000.0))
 }
 
 func TestSumFloat64Go(t *testing.T) {
-	buf := makeBuffer(1000)
+	buf := makeBuffer(10000)
 	res := SumFloat64Go(buf)
-	assert.Equal(t, res, float64(499500.0))
+	assert.Equal(t, res, float64(49995000.0))
 }
 
 func makeBuffer(l int) []float64 {
@@ -31,28 +31,49 @@ func makeBuffer(l int) []float64 {
 	return buf
 }
 
-func BenchmarkSumFloat64AVX(b *testing.B) {
-	buf := makeBuffer(1000)
-	b.SetBytes(int64(len(buf) * 8))
+func BenchmarkSumFloat64AVX_1000(b *testing.B) {
+	benchmarkSumFloat64AVX(b, 1000)
+}
 
+func BenchmarkSumFloat64AVX_10000(b *testing.B) {
+	benchmarkSumFloat64AVX(b, 10000)
+}
+
+func benchmarkSumFloat64AVX(b *testing.B, n int) {
+	buf := makeBuffer(n)
+	b.SetBytes(int64(len(buf) * 8))
 	for i := 0; i < b.N; i++ {
 		SumFloat64AVX(buf)
 	}
 }
 
-func BenchmarkSumFloat64b(b *testing.B) {
-	buf := makeBuffer(1000)
-	b.SetBytes(int64(len(buf) * 8))
+func BenchmarkSumFloat64b_1000(b *testing.B) {
+	benchmarkSumFloat64b(b, 1000)
+}
 
+func BenchmarkSumFloat64b_10000(b *testing.B) {
+	benchmarkSumFloat64b(b, 10000)
+}
+
+func benchmarkSumFloat64b(b *testing.B, n int) {
+	buf := makeBuffer(n)
+	b.SetBytes(int64(len(buf) * 8))
 	for i := 0; i < b.N; i++ {
 		SumFloat64b(buf)
 	}
 }
 
-func BenchmarkSumFloat64Go(b *testing.B) {
-	buf := makeBuffer(1000)
-	b.SetBytes(int64(len(buf) * 8))
+func BenchmarkSumFloat64Go_1000(b *testing.B) {
+	benchmarkSumFloat64Go(b, 1000)
+}
 
+func BenchmarkSumFloat64Go_10000(b *testing.B) {
+	benchmarkSumFloat64Go(b, 10000)
+}
+
+func benchmarkSumFloat64Go(b *testing.B, n int) {
+	buf := makeBuffer(n)
+	b.SetBytes(int64(len(buf) * 8))
 	for i := 0; i < b.N; i++ {
 		SumFloat64Go(buf)
 	}
