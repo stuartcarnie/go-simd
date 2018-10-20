579ad00fb18602d95938602e844c57c534b19f3c
Stuart Carnie <stuart.carnie@gmail.com>
2018-10-20 16:27:15 -0700

    utf8: Add unit tests from Go

--- utf8_avx2_amd64.go
+++ utf8_avx2_amd64.go
@@ -8,10 +8,14 @@ import "unsafe"
 func __validate_utf8_fast_avx2(state, buf, len, res unsafe.Pointer)
 
 func validate_utf8_fast_avx2(v []byte) bool {
+	if len(v) == 0 {
+		return true
+	}
+
 	var (
 		state validateState
 		res   uint64
 	)
 	__validate_utf8_fast_avx2(unsafe.Pointer(&state), unsafe.Pointer(&v[0]), unsafe.Pointer(uintptr(len(v))), unsafe.Pointer(&res))
-	return res > 1
+	return res != 0
 }
--- utf8_test.go
+++ utf8_test.go
@@ -3,6 +3,7 @@
 package simd
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 	"unicode/utf8"
@@ -106,6 +107,53 @@ func TestValidateUTF8Fast(t *testing.T) {
 	res := ValidateUTF8Fast(buf)
 	exp := utf8.Valid(buf)
 	assert.Equal(t, exp, res)
+}
+
+type ValidTest struct {
+	in  string
+	exp bool
+}
+
+var validTests = []ValidTest{
+	{"", true},
+	{"a", true},
+	{"abc", true},
+	{"Ж", true},
+	{"ЖЖ", true},
+	{"брэд-ЛГТМ", true},
+	{"☺☻☹", true},
+	{"aa\xe2", false},
+	{string([]byte{66, 250}), false},
+	{string([]byte{66, 250, 67}), false},
+	{"a\uFFFDb", true},
+	{string("\xF4\x8F\xBF\xBF"), true},      // U+10FFFF
+	{string("\xF4\x90\x80\x80"), false},     // U+10FFFF+1; exp of range
+	{string("\xF7\xBF\xBF\xBF"), false},     // 0x1FFFFF; exp of range
+	{string("\xFB\xBF\xBF\xBF\xBF"), false}, // 0x3FFFFFF; exp of range
+	{string("\xc0\x80"), false},             // U+0000 encoded in two bytes: incorrect
+	{string("\xed\xa0\x80"), false},         // U+D800 high surrogate (sic)
+	{string("\xed\xbf\xbf"), false},         // U+DFFF low surrogate (sic)
+}
+
+func TestValidateUTF8Fast2(t *testing.T) {
+	fnCases := []struct {
+		n  string
+		fn func([]byte) bool
+	}{
+		{"validate_utf8_fast_sse4", validate_utf8_fast_sse4},
+		{"validate_utf8_fast_avx2", validate_utf8_fast_avx2},
+	}
+
+	for _, fn := range fnCases {
+		for _, tc := range validTests {
+			t.Run(fmt.Sprintf("%s(%q)", fn.n, tc.in), func(t *testing.T) {
+				got := fn.fn([]byte(tc.in))
+				if got != tc.exp {
+					t.Errorf("%s(%q) = %v; want %v", fn.n, tc.in, got, tc.exp)
+				}
+			})
+		}
+	}
 }
 
 func BenchmarkValidateUTF8Fast_Go_1000(b *testing.B) {
